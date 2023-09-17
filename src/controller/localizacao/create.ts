2c62bdceb73093c26aa2bfd8ba0a2313661245f5
Mardonio Costa <Mardonio@live.com>
2023-09-17 10:56:11 -0400

    feat: Hash da senha e validação

--- src/controller/localizacao/create.ts
+++ src/controller/localizacao/create.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
@@ -13,7 +14,7 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     cep: z.string(),
     endereco: z.string(),
     whatsapp: z.string(),
-    password_hash: z.string(),
+    password: z.string(),
   });
 
   const {
@@ -26,8 +27,20 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     cep,
     endereco,
     whatsapp,
-    password_hash,
+    password,
   } = registerBodySchema.parse(request.body);
+
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.organizacao.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
 
   await prisma.organizacao.create({
     data: {
